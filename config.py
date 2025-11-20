15d66d7a49a3da53800e14d97ce9a367c9bb3af9
Laumarqz <marquezlautaro16@gmail.com>
2025-11-20 00:10:43 -0300

    Updated

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "AIzaSyDmMaE1rlX53a0VQLN8IrztWCOkvqT6VvY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "ENTER YOUR API KEY HERE")
